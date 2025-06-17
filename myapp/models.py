7d44bb5f81d86be6710267602c5945e49945b336
Juan David Ramón <juanda07070707007@gmail.com>
2025-06-16 19:36:15 -0500

    prueba commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    correo = models.EmailField()
+    edad = models.IntegerField()
+    imagen = models.ImageField(upload_to='imagenes_estudiantes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,4 +126,12 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
